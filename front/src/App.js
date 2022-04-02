c5efb7ff6d3acab2c140ce1aca8c805eff810d61
David Castellanos <49693134+Davidcastel26@users.noreply.github.com>
2022-04-02 13:32:40 -0600

    Update App.js

--- front/src/App.js
+++ front/src/App.js
@@ -1,7 +1,13 @@
 import React from 'react'
 
+// import the container and the components 
+
 import { About, Footer, Header, Skills, Testimonials, Work } from './container'
 import { Navbar } from './components'
+
+// import styles 
+
+import './App.scss'
 
 const App = () => {
   return (
