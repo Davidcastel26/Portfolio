2060a40d2bf41f98cbc2924f6a40adf817c0491a
David Castellanos <49693134+Davidcastel26@users.noreply.github.com>
2022-04-02 15:45:38 -0600

    update

--- front/src/components/Navbar/Navbar.jsx
+++ front/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 
+// styles 
+
 import './Navbar.scss'
+import { images } from '../../constants'
 
 const Navbar = () => {
   return (
-    <nav>
-      <div>
-        <img src={images.log} alt="logo here" />
+    <nav className='app__navbar'>
+      <div className='app__navbar-logo'>
+        <img src={images.logo} alt="logo here" />
+        <ul className='app__navbar-links'>
+          {['home','about','contact','work','skills','testimonials'].map((item)=>(
+            <li className='app_flex p-text' key={`link-${item}`}>
+              <div/>
+              <a href={`#${item}`}>{item}</a>
+            </li>
+          ))}
+        </ul>
       </div>
     </nav>
   )
