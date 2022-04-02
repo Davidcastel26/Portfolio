6fa80dc63df6f7ff018ec3073d1cf01af3fcc7d9
David Castellanos <49693134+Davidcastel26@users.noreply.github.com>
2022-04-02 16:02:45 -0600

    adding menu btn

--- front/src/components/Navbar/Navbar.jsx
+++ front/src/components/Navbar/Navbar.jsx
@@ -1,26 +1,33 @@
-import React from 'react'
+import React from "react";
 
-// styles 
+// styles
 
-import './Navbar.scss'
-import { images } from '../../constants'
+import { HiMenuAlt4, Hix} from 'react-icons/hi'
+import "./Navbar.scss";
+import { images } from "../../constants";
 
 const Navbar = () => {
   return (
-    <nav className='app__navbar'>
-      <div className='app__navbar-logo'>
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
         <img src={images.logo} alt="logo here" />
-        <ul className='app__navbar-links'>
-          {['home','about','contact','work','skills','testimonials'].map((item)=>(
-            <li className='app_flex p-text' key={`link-${item}`}>
-              <div/>
+      </div>
+      <ul className="app__navbar-links">
+        {["home", "about", "contact", "work", "skills", "testimonials"].map(
+          (item) => (
+            <li className="app_flex p-text" key={`link-${item}`}>
+              <div />
               <a href={`#${item}`}>{item}</a>
             </li>
-          ))}
-        </ul>
+          )
+        )}
+      </ul>
+
+      <div className="app__navbar-menu">
+
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
