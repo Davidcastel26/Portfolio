2cac53003ba3ec7c9121ad5ab2f149a33f45a93f
David Castellanos <49693134+Davidcastel26@users.noreply.github.com>
2022-04-03 15:04:48 -0600

    Update client.js

--- front/src/client.js
+++ front/src/client.js
@@ -2,9 +2,9 @@ import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url'
 
 export const client = sanityClient({
-  projectId:'',
-  dataset:'',
-  apiVersion:'',
+  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+  dataset:'production',
+  apiVersion:'2022-04-03',
   useCdn:true,
-  token:''
+  token: process.env.REACT_APP_SANITY_TOKEN
 })
