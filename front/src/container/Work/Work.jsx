60d340b219fbe5a7e8965d143c733a7fe63d2409
David Castellanos <49693134+Davidcastel26@users.noreply.github.com>
2022-04-05 16:32:46 -0600

    Update Work.jsx

--- front/src/container/Work/Work.jsx
+++ front/src/container/Work/Work.jsx
@@ -1,77 +1,100 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from "react";
 
 // styles
-import './Work.scss';
-import { AiFillEye, AiFillGithub} from 'react-icons/ai';
-import { motion } from 'framer-motion';
-// components 
-import {AppWrap} from '../../wrapper'
+import "./Work.scss";
+import { AiFillEye, AiFillGithub } from "react-icons/ai";
+import { motion } from "framer-motion";
+// components
+import { AppWrap } from "../../wrapper";
 // back sanity
 import { urlFor, client } from "../../client";
 
 const Work = () => {
+  // use State
+  const [activeFilter, setActiveFilter] = useState("All");
+  const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
+  const [works, setWorks] = useState([]);
+  const [filterWork, setFilterWork] = useState([]);
 
-  // use State 
-  const [activeFilter, setActiveFilter] = useState('All')
-  const [animateCard, setAnimateCard ] = useState({ y : 0, opacity: 1 });
-  const [works, setWorks] = useState([])
-  const [filterWork, setFilterWork] = useState([])
-
-  // use Effect 
-  useEffect(()=>{
+  // use Effect
+  useEffect(() => {
     const query = '*[_type == "works"]';
 
-    client.fetch(query)
-      .then((data)=>{
-        setWorks(data)
-        setFilterWork(data)
-      })
-  },[])
+    client.fetch(query).then((data) => {
+      setWorks(data);
+      setFilterWork(data);
+    });
+  }, []);
 
-  const handleWorkFilter = (item) => { }
+  const handleWorkFilter = (item) => {};
 
   return (
     <>
-      <h2 className="head-text">My Creative <span> Portfolio </span> Section</h2>
+      <h2 className="head-text">
+        My Creative <span> Portfolio </span> Section
+      </h2>
 
       <div className="app__work-filter">
-        {['UI/UX', 'Web App', 'React JS', 'All'].map((item,index) => (
+        {["UI/UX", "Web App", "React JS", "All"].map((item, index) => (
           <div
             key={index}
             onClick={() => handleWorkFilter(item)}
-            className={`app__work-filter-item app__flex p-text ${activeFilter  === item ? 'item-active' : '' }`}
+            className={`app__work-filter-item app__flex p-text ${
+              activeFilter === item ? "item-active" : ""
+            }`}
           >
             {item}
           </div>
-          
         ))}
       </div>
       <motion.div
         animate={animateCard}
-        transition={{duration: 0.5, delayChildren: 0.5}}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
         className="app__work-portfolio"
       >
-        {works.map((work, index)=>(
-         
+        {works.map((work, index) => (
           <div className="app__work-item app__flex" key={index}>
             <div className="app__work-img app__flex">
               <img src={urlFor(work.imgUrl)} alt={work.name} />
               <motion.div
-                whileHover={{opacity: [0, 1]}}
-                transition={{duration:0.25, ease:'easeInOut', staggerChildren: 0.5}}
+                whileHover={{ opacity: [0, 1] }}
+                transition={{
+                  duration: 0.25,
+                  ease: "easeInOut",
+                  staggerChildren: 0.5,
+                }}
                 className="app__work-hover app__flex"
               >
-                <a href={work.projectLink} alt='projectLink' target="_blank" rel='noreferrer'></a>
+                <a
+                  href={work.projectLink}
+                  alt="projectLink"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <motion.div
+                    whileInView={{scale: [0, 1]}}
+                    whileHover={{ scale: [1, 0.9]}}
+                    transition={{ duration: 0.25 }}
+                    className="app__flex"
+                  >
+                    <AiFillEye />
+                  </motion.div>
+                  <motion.div
+                    whileInView={{scale: [0, 1]}}
+                    whileHover={{ scale: [1, 0.9]}}
+                    transition={{ duration: 0.25 }}
+                    className="app__flex"
+                  >
+                    <AiFillGithub />
+                  </motion.div>
+                </a>
               </motion.div>
-             {
-              console.log(` ${urlFor(work.imgUrl)} + WORK`)
-             }
             </div>
           </div>
         ))}
       </motion.div>
     </>
-  )
-}
+  );
+};
 
-export default Work
+export default Work;
