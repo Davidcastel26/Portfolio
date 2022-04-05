040897047050be31ac9199bbe4760ef28bf64352
David Castellanos <49693134+Davidcastel26@users.noreply.github.com>
2022-04-05 10:41:59 -0600

    Update SocialMedia.jsx

--- front/src/components/SocialMedia.jsx
+++ front/src/components/SocialMedia.jsx
@@ -5,7 +5,15 @@ import {BsLinkedin, BsGithub, BsInstagram } from 'react-icons/bs'
 const SocialMedia = () => {
   return (
     <div className='app__social'>
-      
+      <div>
+        <BsGithub />
+      </div>
+      <div>
+        <BsLinkedin />
+      </div>
+      <div>
+        <BsInstagram />
+      </div>
     </div>
   )
 }
