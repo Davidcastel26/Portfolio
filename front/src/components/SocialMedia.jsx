7095912bb05e4bd3cdd825e3ac8df38cfc3bf8ca
David Castellanos <49693134+Davidcastel26@users.noreply.github.com>
2022-04-05 10:40:27 -0600

    Create SocialMedia.jsx

---
+++ front/src/components/SocialMedia.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import {BsLinkedin, BsGithub, BsInstagram } from 'react-icons/bs'
+
+const SocialMedia = () => {
+  return (
+    <div className='app__social'>
+      
+    </div>
+  )
+}
+
+export default SocialMedia
