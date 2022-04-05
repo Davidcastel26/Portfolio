0f00aaece97c912e46048b23b18eff657b6e91c1
David Castellanos <49693134+Davidcastel26@users.noreply.github.com>
2022-04-05 11:27:08 -0600

    Update AppWrap.js

--- front/src/wrapper/AppWrap.js
+++ front/src/wrapper/AppWrap.js
@@ -2,9 +2,21 @@ import React from 'react'
 
 import { NavigationDots, SocialMedia } from '../components'
 
-const AppWrap = () => {
+const AppWrap = (Componet, idName, classNames) => function HOC() {
   return (
-    <div>AppWrap</div>
+    <div id={idName} className={`app__container ${classNames}`}>
+
+      <SocialMedia />
+      <div className="app__wrapper app__flex">
+        <Componet />
+      </div>
+
+      <div className="copyright">
+        <p className="p-text">@2022 Dave</p>
+        <p className="p-text">All rights reserved</p>
+      </div>
+
+    </div>
   )
 }
 
