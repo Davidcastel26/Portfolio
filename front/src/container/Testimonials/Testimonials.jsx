b2602c07605ac162c041431bd2f3b61e07af636d
David Castellanos <49693134+Davidcastel26@users.noreply.github.com>
2022-04-09 17:08:29 -0600

    Update Testimonials.jsx

--- front/src/container/Testimonials/Testimonials.jsx
+++ front/src/container/Testimonials/Testimonials.jsx
@@ -1,5 +1,13 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+
+// styles
 import './Testimonials.scss'
+import ReactTooltip from "react-tooltip";
+import { motion } from "framer-motion";
+// components
+import { AppWrap, MotionWrap } from "../../wrapper";
+// back sanity
+import { urlFor, client } from "../../client";
 
 const Testimonials = () => {
   return (
