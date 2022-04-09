b432c59f5c855f6f139f96fbe8c3572a5e6342d9
David Castellanos <49693134+Davidcastel26@users.noreply.github.com>
2022-04-08 20:20:42 -0600

    Update Skills.jsx

--- front/src/container/Skills/Skills.jsx
+++ front/src/container/Skills/Skills.jsx
@@ -3,7 +3,6 @@ import React, { useEffect, useState } from "react";
 // styles
 import './Skills.scss'
 import ReactTooltip from 'react-tooltip'
-import { AiFillEye, AiFillGithub } from "react-icons/ai";
 import { motion } from "framer-motion";
 // components
 import { AppWrap } from "../../wrapper";
@@ -11,9 +10,49 @@ import { AppWrap } from "../../wrapper";
 import { urlFor, client } from "../../client";
 
 const Skills = () => {
+
+  const [experiences, setExperiences] = useState([])
+  const [skills, setSkills] = useState([])
+  
+  useEffect(() => {
+    const skillsQuery = '*[_type == "skills"]';
+    const query = '*[_type == "experiences"]';
+
+    client.fetch(query)
+      .then((data) => {
+        setExperiences(data)
+        
+      })
+    client.fetch(skillsQuery)
+      .then((data) => {
+        setSkills(data)
+        
+      })
+  }, [])
+
   return (
     <>
-      <p>Hello</p>
+      <h2 className="head-text">Skills & Experience</h2>
+
+      <div className="app__skills-container">
+        <motion.div className="app__skils-list">
+          {
+            skills.map((skill) => (
+              <motion.div
+                whileInView={{opacity: [0, 1]}}
+                transition={{ duration: 0.5}}
+                className="app__skills-item app__flex"
+                key={skill.name}
+              >
+                <div className="app__flex" style={{backgroundColor:  skill.bgColor}}>
+                  <img src={urlFor(skill.icon)} alt={skill.name} />
+                </div>
+                <p className="p-text">{skill.name}</p>
+              </motion.div>
+            ))
+          }
+        </motion.div>
+      </div>
     </>
   )
 }
