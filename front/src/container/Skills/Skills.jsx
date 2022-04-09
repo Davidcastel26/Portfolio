a0170ae19c61ac29fd4b797f570e1cdda5911f3e
David Castellanos <49693134+Davidcastel26@users.noreply.github.com>
2022-04-08 21:08:51 -0600

    Update Skills.jsx

--- front/src/container/Skills/Skills.jsx
+++ front/src/container/Skills/Skills.jsx
@@ -53,7 +53,22 @@ const Skills = () => {
           }
         </motion.div>
         <motion.div className="app__skills-exp">
-
+          {
+            experiences.works.map((work) => (
+              <>
+                <motion.div
+                  whileInView={{opacity: [0, 1]}}
+                  transition={{duration: 0.5}}
+                  className="app__skills-exp-work"
+                  data-tip
+                  data-for={work.name}
+                  key={work.name}
+                  >
+                    <h4 className="bold-text">{work.name}</h4>
+                </motion.div>
+              </>
+            ))
+          }
         </motion.div>
       </div>
     </>
