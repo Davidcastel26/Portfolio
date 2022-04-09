7ad4836cefad6e71adc4c44910fd2679e7b31d0f
David Castellanos <49693134+Davidcastel26@users.noreply.github.com>
2022-04-09 17:16:38 -0600

    Update Testimonials.jsx

--- front/src/container/Testimonials/Testimonials.jsx
+++ front/src/container/Testimonials/Testimonials.jsx
@@ -4,14 +4,36 @@ import React, { useEffect, useState } from "react";
 import './Testimonials.scss'
 import ReactTooltip from "react-tooltip";
 import { motion } from "framer-motion";
+import { HiChevronLeft, HiChevronRight } from 'react-icons/hi'
 // components
 import { AppWrap, MotionWrap } from "../../wrapper";
 // back sanity
 import { urlFor, client } from "../../client";
 
 const Testimonials = () => {
+
+  const [brands, setBrands] = useState([])
+  const [testimonials, setTestimonials] = useState([])
+  const [currentIndex, setCurrentIndex] = useState(0)
+
+
+  useEffect( () => {
+    const query = '*[_type == "testimonials"]';
+    const brandsQuery = '*[_type == "brands"]';
+
+    client.fetch(query)
+      .then((data) => {
+        setTestimonials(data)
+      })
+
+    client.fetch(brandsQuery)
+      .then((data) =>{
+        setBrands(data)
+      })   
+  })
+
   return (
-    <div>Testimonials</div>
+    <div></div>
   )
 }
 
