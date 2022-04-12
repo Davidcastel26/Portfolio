350db5ce41f9aae8a22e71c431b8b149e97ccd85
David Castellanos <49693134+Davidcastel26@users.noreply.github.com>
2022-04-12 17:11:32 -0600

    Update Footer.jsx

--- front/src/container/Footer/Footer.jsx
+++ front/src/container/Footer/Footer.jsx
@@ -4,12 +4,35 @@ import React, {useState, useEffect} from 'react'
 import './Footer.scss'
 import { images } from '../../constants'
 import {AppWrap, MotionWrap } from '../../wrapper'
+// backend 
+import { client } from '../../client'
 
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <>
+      <h2 className="head-text">Take a coffee & chat with me</h2>
+      <div className="app__footer-cards">
+        <div className="app__footer-card">
+          <img src={images.email} alt="email"/>
+          <a href="mailto: davcastellanoslarios@gmail.com" className='p-text'>davcastellanoslarios@gmail.com</a>
+        </div>
+        <div>
+        <img src={images.mobile} alt="mobile"/>
+          <a href="tel: +502 3743 1717" className='p-text'>+502 3743 1717</a>
+        </div>
+      </div>
+
+      <div className="app__footer-form app__flex">
+        <div className="app__flex">
+          <input type="text" className="p-text" placeholder='Your Name' />
+        </div>
+      </div>
+    </>
   )
 }
 
-export default Footer
+export default AppWrap(
+  MotionWrap(Footer, 'app__footer'),
+  'app__whitebg'
+) 
