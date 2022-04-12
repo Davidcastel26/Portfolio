f0fef97a4a62b3ef0496ecb5858c5c67acb96d5e
David Castellanos <49693134+Davidcastel26@users.noreply.github.com>
2022-04-12 17:01:27 -0600

    Update Footer.jsx

--- front/src/container/Footer/Footer.jsx
+++ front/src/container/Footer/Footer.jsx
@@ -2,6 +2,9 @@ import React, {useState, useEffect} from 'react'
 
 // STYLES  
 import './Footer.scss'
+import { images } from '../../constants'
+import {AppWrap, MotionWrap } from '../../wrapper'
+
 
 const Footer = () => {
   return (
